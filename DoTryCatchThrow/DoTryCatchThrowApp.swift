548d0f82064542c2d31702d6a5aaefaa01cab142
Christian Skorobogatow <christiansko@icloud.com>
2022-07-21 23:47:59 +0200

    Initial Commit

---
+++ DoTryCatchThrow/DoTryCatchThrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTryCatchThrowApp.swift
+//  DoTryCatchThrow
+//
+//  Created by Christian Skorobogatow on 21/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DoTryCatchThrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
