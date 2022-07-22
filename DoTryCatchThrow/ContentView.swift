2d008e85fbf22402bb679292fb71c091f02b5517
Christian Skorobogatow <christiansko@icloud.com>
2022-07-22 11:27:54 +0200

    feat(): example why we use try do catch throw

--- DoTryCatchThrow/ContentView.swift
+++ DoTryCatchThrow/ContentView.swift
@@ -7,10 +7,111 @@
 
 import SwiftUI
 
+class DoCatchTryThrowDataManager {
+    
+    static let instance = DoCatchTryThrowDataManager()
+    private init() {}
+    
+    let isActive: Bool = true
+    
+    func getTitle() -> (title: String?, error: Error?) {
+        if isActive {
+            return ("NEW TEXT", nil)
+        } else {
+            return (nil, URLError(.badURL))
+        }
+    }
+    
+    func getTitle2() -> Result<String, Error> {
+        if isActive {
+            return .success("NEW TEXT!")
+        } else {
+            return .failure(URLError(.badURL))
+        }
+    }
+    
+    func getTitle3() throws -> String {
+//        if isActive {
+//            return "NEW TEXT!"
+//        } else {
+            throw URLError(.badServerResponse)
+//        }
+    }
+    
+    func getTitle4() throws -> String {
+        if isActive {
+            return "FINAL TEXT!"
+        } else {
+            throw URLError(.badServerResponse)
+        }
+    }
+}
+
+
+class DoCatchTryThrowViewModel: ObservableObject {
+    @Published var text: String = "Starting text..."
+    let manager = DoCatchTryThrowDataManager.instance
+    
+    
+    func fetchTitle () {
+        /*
+        let returnedValue = manager.getTitle()
+        if let newTitle = returnedValue.title {
+            self.text = newTitle
+        } else if let error = returnedValue.error {
+            self.text = error.localizedDescription
+        }
+         */
+        
+        /*
+        let result = manager.getTitle2()
+        
+        
+        switch result {
+        case .success(let newTitle):
+            self.text = newTitle
+        case .failure(let error):
+            self.text = error.localizedDescription
+        }
+         */
+        
+        /*
+        let newTitle = try? manager.getTitle3()
+        if let newTitle = newTitle {
+            self.text = newTitle
+        }
+         */
+        
+        
+        
+        
+        do {
+            let newTitle = try? manager.getTitle3()
+            if let newTitle = newTitle {
+                self.text = newTitle
+            }
+            
+            let finalTitle = try manager.getTitle4()
+            self.text = finalTitle
+            
+        } catch let error {
+            self.text = error.localizedDescription
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @StateObject var viewModel = DoCatchTryThrowViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(viewModel.text)
+            .frame(width: 300, height: 300)
+            .background(.blue)
             .padding()
+            .onTapGesture {
+                viewModel.fetchTitle()
+            }
     }
 }
 
